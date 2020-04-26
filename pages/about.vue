44f2046e5421dd5d191c1a0819a2d688d836935b
InnaPodkosova <innapodkosova@gmail.com>
2020-04-26 12:48:45 +0300

    start project

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+   <section>
+     <h1>About page</h1>
+     <p>lorem ipsum
+     </p>
+   </section>
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
+
+<style scoped>
+
+</style>
